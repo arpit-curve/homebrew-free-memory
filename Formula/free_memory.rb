f3c30e788440628b8ab7e0e60d385ce203da7a48
Administrator <admin@example.com>
2025-01-30 21:24:39 +0530

    updated checksum

--- Formula/free_memory.rb
+++ Formula/free_memory.rb
@@ -2,7 +2,7 @@ class ToggleHistory < Formula
   desc "Interactive script to enable or disable command history tracking"
   homepage "https://github.com/arpit-curve/homebrew-free-memory"
   url "https://github.com/arpit-curve/homebrew-free-memory/archive/v1.0.0.tar.gz"
-  sha256 "a435f9a36cbddd3f658b82dc92867539c4140f52cc410c4b73f189d9be712c42"
+  sha256 "781df07034856b2a8473da3594a96239a4e9fedd83be76935d6226637eba7351"
   license "MIT"
 
   def install
